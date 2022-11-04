d31abf4dd8c75dbd73f97387d0df7de5a134cbf2
CoreyThuro <77815068+CoreyThuro@users.noreply.github.com>
2022-11-04 12:15:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const main_video = document.querySelector('.main-video video')
 const main_video_title = document.querySelector('.main-video .title')
 const video_playlist = document.querySelector('.video-playlist .videos')
+const progress = document.querySelector('.progress')
 
 let data = [
     {
         id: 1,
-        title: 'Beef Ring',
+        title: 'Beef ring',
         name: "BeefRing1960_2_512kb.mp4",
         duration: "11:05"
     },
@@ -17,7 +18,7 @@ let data = [
     },
     {
         id: 3,
-        title: 'What about prejudice.',
+        title: 'What about prejudice',
         name: 'WhatAbou1959_512kb.mp4',
         duration: "18:15"
     },
@@ -28,6 +29,16 @@ let data = [
         duration: "26:02"
     }
 ]
+
+let section = {
+    module1: {id: 1, isComplete: false},
+    module2: {id: 2, isComplete: false},
+    module3: {id: 3, isComplete: false},
+    module4: {id: 4, isComplete: false},
+    key: function(n) {
+        return this[Object.keys(this)[n]];
+    }
+}
 
 data.forEach((video) => {
     let video_element = `
@@ -46,32 +57,66 @@ videos[0].classList.add('active')
 videos.forEach(selected_video => {
     selected_video.onclick = () => {
     let match_video = data.find(video => video.id == selected_video.dataset.id)
-    console.log(match_video)
+  
     main_video.src = 'assets/videos/' + match_video.name
     main_video.title =  match_video.title;
     main_video.id = match_video.id;
+    
+   nextVideo()
     }
-
 })
 
-    let currentVideo = main_video.id;
 
     function nextVideo(){
-        console.log('ended')
-
-
-
-        console.log(main_video.id)
-        let next_video = data.find(video => video.id == currentVideo)
-        console.log(next_video)
-           
-
-      
-             main_video.src = 'assets/videos/' + next_video.name
-            main_video.title =  next_video.title;
-            currentVideo++
-            main_video.addEventListener('ended', nextVideo,false);
+        main_video.removeEventListener('ended', nextVideo)
         
+        let match_video = data.find(video => video.id == main_video.id)
+        let main_id_int = parseInt(main_video.id)
+
+        console.log(match_video)
+
+        if(main_id_int >= data.length){
+            main_video.id = 1;
+        } else {
+            main_video.id++
+        }
+
+        main_video.src = 'assets/videos/' + match_video.name
+        main_video.title =  match_video.title;
+       
+        main_video.addEventListener('ended', nextVideo, false);
+        calculateProgress()
     }
 
+    function updateModuleCompletion(){
+        let main_vid_id = parseInt(main_video.id, 10)
+
+        if(main_vid_id <= data.length){
+           
+            let vid_module = section.key(main_vid_id-1)
+            vid_module.isComplete = true;
+          
+        } else {
+            let lastModule = (Object.keys(section).length)-2;
+            let vid_module = section.key(lastModule)
+            vid_module.isComplete = true;
+        }
+    
+    }
+
+    function calculateProgress(){
+        let numberComplete = 0;
+
+        for(moduleN in section){
+            if(section[moduleN].isComplete){
+                numberComplete++
+            }
+        }
+        let percentageComplete = (numberComplete/(Object.keys(section).length-1)*100)
+        progress.innerHTML = `Progress Complete: &nbsp${percentageComplete}%`;
+       
+    }
+
+main_video.addEventListener('ended', updateModuleCompletion, false);
 nextVideo()
+
